6e6612b434933e3ccb36075442eb100c6674e9f2
Samyak Jain <samyaklearner@gmail.com>
2020-09-09 21:19:33 +0530

    users CRUD operation implemented

--- Activity/model/connection.js
+++ Activity/model/connection.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     // db password
-    password: 'bpit9868',
+    password: 'password',
     // db instance name 
     database: 'socialize'
 })
