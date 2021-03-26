7e1a912e8d547c5e376f99d7c7ad3486109c20fe
Samyak Jain <samyaklearner@gmail.com>
2021-03-27 00:09:22 +0530

    Update connection.js

--- Activity/model/connection.js
+++ Activity/model/connection.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     // db password
-    password: 'bpit9868',
+    password: '12345678',
     // db instance name 
     database: 'socialize'
 })
